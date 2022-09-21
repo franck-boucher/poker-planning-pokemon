0ff387b2603fcb6c215b6b0adbf742f586e3109c
Franck Boucher <boucher.fk@gmail.com>
2022-09-22 00:48:47 +0200

    Small UI adjustment

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -54,7 +54,7 @@ export const Home = () => {
         </div>
 
         <div className="bg-gray-200 p-6 md:p-16 w-full rounded-md flex flex-col gap-4 mb-10 items-center justify-center">
-          <div className="flex gap-4">
+          <div className="flex gap-2 sm:gap-4">
             {randomPokemonIds.map((id, idx) => (
               <img
                 key={id}
