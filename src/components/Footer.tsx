186a2deb1f31bf480b40c3c6d7fba74819561e26
Franck Boucher <boucher.fk@gmail.com>
2022-10-06 22:16:26 +0200

    Add a footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+export const Footer = () => (
+  <footer className="flex gap-2 justify-center">
+    <span>
+      Made by{" "}
+      <a className="font-bold" href="https://fboucher.com/" target="_blank">
+        Franck boucher
+      </a>
+    </span>
+  </footer>
+);
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,9 +1,13 @@
 import { ReactNode } from "react";
+import { Footer } from "./Footer";
 
 export const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <main className="flex flex-col items-center w-full h-full container mx-auto p-4 md:p-8">
-      <div className="flex flex-col w-full h-full">{children}</div>
+      <div className="flex flex-col w-full h-full">
+        {children}
+        <Footer />
+      </div>
     </main>
   );
 };
