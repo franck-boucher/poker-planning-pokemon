b6feac4b3fa99de56b9efd8bf061e5a8a15648b4
Franck Boucher <boucher.fk@gmail.com>
2022-10-12 22:50:29 +0200

    Update footer with GitHub link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,13 +3,21 @@ import { useTranslation } from "react-i18next";
 export const Footer = () => {
   const { t } = useTranslation();
   return (
-    <footer className="flex gap-2 justify-center">
+    <footer className="flex gap-2 justify-center text-sm">
       <span>
         {t("footer.madeBy")}{" "}
         <a className="font-bold" href="https://fboucher.com/" target="_blank">
           Franck Boucher
         </a>
       </span>
+      ·
+      <a
+        className="font-bold"
+        href="https://github.com/franck-boucher/poker-planning-pokemon"
+        target="_blank"
+      >
+        GitHub
+      </a>
     </footer>
   );
 };
