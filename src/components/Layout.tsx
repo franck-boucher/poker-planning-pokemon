851f81fa930e578669be92f29516702960d19941
Franck Boucher <boucher.fk@gmail.com>
2022-10-12 23:02:44 +0200

    Push footer to the bottom on all pages

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,7 +5,7 @@ export const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <main className="flex flex-col items-center w-full h-full container mx-auto p-4 md:p-8">
       <div className="flex flex-col w-full h-full">
-        {children}
+        <div className="flex flex-col flex-1">{children}</div>
         <Footer />
       </div>
     </main>
